f4f1184707eea9729ca50a14583a2a471ba32026
akrito <vipchenpost@163.com>
2020-02-29 21:25:08 +0800

    try

--- src/main/java/com/github/hcsp/inheritance/Boy.java
+++ src/main/java/com/github/hcsp/inheritance/Boy.java
@@ -1,17 +1,22 @@
 package com.github.hcsp.inheritance;
 
 public class Boy {
-    private String name;
     private int age;
 
+    //这里我想用组合的方式 先用sayMyName这个函数试试
+    SayName sayName;
+
+    public void sayMyName() {
+        sayName.sayMyName();
+    }
+
+    //因为我把name放进了sayName里面，这样调用就有空指针的问题了，用了if判断也不对
+    //这个如果使用继承的话可以用super语法，但是用组合又该怎么处理呢？
     public Boy(String name, int age) {
-        this.name = name;
+        sayName.name = name;
         this.age = age;
     }
 
-    public void sayMyName() {
-        System.out.println("我的名字是" + name);
-    }
 
     public void sayMyAge() {
         System.out.println("我的年龄是" + age);
